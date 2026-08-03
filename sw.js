28869f5abd381fee1c201b1ce16ba469f308226c
SpartanTaylor <jamesaarontaylor@hotmail.co.uk>
2026-08-04 00:47:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="weighted-chinup-master-v8-unit-toggle";
+const CACHE="weighted-chinup-master-v9-lbs-label";
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>
